76af6944bc1e33e1590e11a03a82d3bc86ff14f4
Kseniia_Liepokhina <kseniya.lepiohina@gmail.com>
2026-01-23 22:15:53 +0200

    added settings for frontend connection

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import * as express from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Weather forecast')
+    .setDescription('The weather forecast API description')
+    .setVersion('1.0')
+    .addTag('weather, OpenWeatherApi')
+    .build()
+    const documentFactory = () => SwaggerModule.createDocument(app,config);
+    SwaggerModule.setup('api', app,documentFactory);
   app.enableCors({
           origin: 'https://weather-app-one-steel-71.vercel.app',
           credentials: true,
       });
 
-  await app.listen(process.env.PORT ?? 5000);
+  await app.listen('https://weather-app-backend-agme.onrender.com');
 
 }
 bootstrap();
