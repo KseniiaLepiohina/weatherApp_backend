70c24489d93100705487de623848424744e1eee1
Kseniia_Liepokhina <76071580+KseniiaLepiohina@users.noreply.github.com>
2026-02-12 11:09:02 +0200

    Delete dist directory (#4)

--- dist/weather-data/weather-data.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { WeatherDataService } from './weather-data.service';
-export declare class WeatherDataController {
-    private readonly weatherDataService;
-    constructor(weatherDataService: WeatherDataService);
-    getWeatherDataByLocation(lat: string, lng: string): Promise<any>;
-    getWeatherForecastByLocation(lat: string, lng: string): Promise<any>;
-    weatherByCity(cityname: string, countrycode: string): Promise<any>;
-    forecastByCity(cityname: string, countrycode: string): Promise<any>;
-}
